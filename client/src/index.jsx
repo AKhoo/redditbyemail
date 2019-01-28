81283bb56d8cf4bd2ae0fd1284811a550896bcdc
Adrian Khoo <adriankhoo.ca@gmail.com>
2019-01-27 18:11:36 -0800

    Expanded categories and subs on front end

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -32,25 +32,120 @@ class App extends React.Component {
     this.state = { 
       email: '',
       categories: {
-        'General Knowledge': {
-          name: 'General Knowledge',
+        'Interesting Facts': {
+          name: 'Interesting Facts',
           checked: true,
           order: 1,
           subs: {
             'TodayILearned': true,
-            'ExplainLikeImFive': false
+            'ExplainLikeImFive': true,
+            'Wikipedia': true,
           },
+        },
+        'Business & Technology': {
+          name: 'Business & Technology',
+          checked: true,
+          order: 2,
+          subs: {
+            'Business': true,
+            'Futurology': true,
+            'Gadgets': true,
+            'Technology': true,
+          }
+        },
+        'Science': {
+          name: 'Science',
+          checked: true,
+          order: 3,
+          subs: {
+            'Economics': true,
+            'Engineering': true,
+            'History': true,
+            'Philosophy': true,
+            'Psychology': true,
+            'Science': true,
+            'Space': true,
+          }
+        },
+        'Quotes': {
+          name: 'Quotes',
+          checked: true,
+          order: 4,
+          subs: {
+            'Quotes': true,
+            'QuotesPorn': true,
+          }
+        },
+        'Entertaining Reads': {
+          name: 'Entertaining Reads',
+          checked: true,
+          order: 5,
+          subs: {
+            'AskReddit': true,
+            'WritingPrompts': true,
+          }
         },
         'JavaScript': {
           name: 'JavaScript',
           checked: true,
-          order: 1,
+          order: 6,
           subs: {
+            'DailyProgrammer': true,
+            'FrontEnd': true,
             'JavaScript': true,
             'Node': true,
             'ReactJS': true,
+            'WebDev': true,
+            'Web_Design': true,
           }
-        }
+        },
+        'Life Hacks': {
+          name: 'Life Hacks',
+          checked: true,
+          order: 7,
+          subs: {
+            'Lifehacks': true,
+            'LifeProTips': true,
+          }
+        },
+        'Personal Finance': {
+          name: 'Personal Finance',
+          checked: true,
+          order: 8,
+          subs: {
+            'Investing': true,
+            'PersonalFinance': true,
+          }
+        },
+        'Self Improvement': {
+          name: 'Self Improvement',
+          checked: true,
+          order: 9,
+          subs: {
+            'GetMotivated': true,
+            'Productivity': true,
+            'SelfImprovement': true,
+            'ZenHabits': true,
+          }
+        },
+        'Worth Watching': {
+          name: 'Worth Watching',
+          checked: true,
+          order: 10,
+          subs: {
+            'Documentaries': true,
+            'NetflixBestOf': true,
+          }
+        },
+        'Recipes': {
+          name: 'Recipes',
+          checked: true,
+          order: 11,
+          subs: {
+            'GifRecipes': true,
+            'Recipes': true,
+          }
+        },
       },
       posts: {},
       display: {},
