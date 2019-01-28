638bbedd6d07d177d3734fff1afde09f88f902f4
Adrian Khoo <adriankhoo.ca@gmail.com>
2019-01-28 11:22:43 -0800

    Emails send with content but no styling

--- server/email.js
+++ server/email.js
@@ -1,5 +1,8 @@
 const nodemailer = require('nodemailer');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
 const env = require('../.env');
+const User = require('../db/user');
 const Email = require('../client/dist/bundle-ssr');
 
 const transport = nodemailer.createTransport({
@@ -12,24 +15,34 @@ const transport = nodemailer.createTransport({
   },
 });
 
-// For each user
+// For each user, get their posts by sub and render email component
+User.getAll((users) => {
+  users.forEach((user) => {
+    User.getPostsBySubCollection(user.email, (allPosts) => {
+      const randomSub = Object.keys(allPosts)[Math.floor(Math.random() * Object.keys(allPosts).length)];
+      const randomTopPost = allPosts[randomSub][0].title;
+      console.log('Random Top Post:', randomTopPost);
+      const email = React.createElement(Email, { categoriesSelected: Object.entries(allPosts) });
+
+      const mailOptions = {
+        from: "Reddit By Email <adriankhoo.ca@gmail.com>", // sender address
+        to: "Adrian <adriankhoo.ca@gmail.com>", // list of receivers
+        subject: randomTopPost, // Subject line
+        html: ReactDOMServer.renderToString(email), // email body
+      };
+
+      transport.sendMail(mailOptions, (error, response) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Message sent: " + response.message);
+        }
+      });
+    })
+  })
+})
   // Get their relevant posts
   // Format to shape expected by Email component
   // Render email component
 
-const mailOptions = {
-  from: "Adrian <adriankhoo.ca@gmail.com>", // sender address
-  to: "Adrian <adriankhoo.ca@gmail.com>", // list of receivers
-  subject: "Test Subject", // Subject line
-  html: "<b>TEST BODY!!</b>" // email body
-};
-
-transport.sendMail(mailOptions, (error, response) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Message sent: " + response.message);
-  }
-
-  transport.close(); // shut down the connection pool, no more messages
-});
+  // transport.close(); // shut down the connection pool, no more messages
